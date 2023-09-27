b061c0f439015a2fe8745794a4321ee64434bb5a
MaxWebb96 <maxwebb0825@outlook.com>
2023-09-27 11:06:52 -0400

    implement the inheritance and delegation

--- Java/src/main/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntList.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntList.java
@@ -10,7 +10,52 @@ package edu.cmu.cs.cs214.rec04;
  *
  */
 
-public class DelegationSortedIntList {
+public class DelegationSortedIntList implements IntegerList{
     // Write your implementation below with API documentation
+
+    private SortedIntList list;
+    private int totalAdded = 0;
+
+    public DelegationSortedIntList(){
+        list = new SortedIntList();
+    }
+
+    @Override
+    public boolean add(int num){
+
+        totalAdded++;
+        return list.add(num);
+    }
+
+    @Override
+    public boolean addAll(IntegerList list){
+
+        totalAdded += list.size();
+        return this.list.addAll(list);
+    }
+
+    public int getTotalAdded(){
+        return totalAdded;
+    }
+
+    @Override
+    public int size(){
+        return list.size();
+    }
+
+    @Override
+    public int get(int index){
+        return list.get(index);
+    }
+
+    @Override
+    public boolean remove(int num){
+        return list.remove(num);
+    }
+
+    @Override
+    public boolean removeAll(IntegerList list){
+        return this.list.removeAll(list);
+    }
 
 }
